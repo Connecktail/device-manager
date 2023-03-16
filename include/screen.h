eebad35a3c1e46b4464e14ac7d09d16a1a59ce99
LfrPaul <78427088+LfrPaul@users.noreply.github.com>
2023-03-16 20:15:09 +0100

    Screen app - Homepage (#2)
    * init screen thread
    homepage
    
    * adapt window for different dimensions
    
    * always show scrollbar
    
    ---------
    
    Co-authored-by: Paul Lefevre <p.lefevre@compethance.com>

---
+++ include/screen.h
@@ -0,0 +1,26 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <gtk-3.0/gtk/gtk.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <sys/mman.h>
+#include <db-utils/dbutils.h>
+
+#define db_host "127.0.0.1"
+#define db_database "connecktail"
+#define db_user "admin"
+#define db_password "admin"
+
+/**
+ * @brief thread function to display the screen app and listen to screen interaction
+ */
+void *display_screen(void *arg);
+
+#endif
