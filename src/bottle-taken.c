1d38a379a367c7a875b922e775557b3a1ba2a3f8
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2023-05-12 00:11:55 +0200

    [ADD] call to update_current_order on signal SIGPROF

--- src/bottle-taken.c
+++ src/bottle-taken.c
@@ -1,6 +1,8 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <msq-utils/msqutils.h>
 #include "../include/bottle-taken.h"
+#include "../include/screen.h"
 
 pthread_cond_t bottle_taken_condition = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex_bottle_taken_condition = PTHREAD_MUTEX_INITIALIZER;
@@ -11,8 +13,9 @@ void handle_bottle_taken(){
     pthread_mutex_init(&mutex_bottle_taken_condition, NULL);
 
     while(1){
-        printf("waiting for signal\n");
         pthread_cond_wait(&bottle_taken_condition, &mutex_bottle_taken_condition);
-        printf("Bottle taken\n");
+        update_current_order();
     }   
+
+    pthread_exit(NULL);
 }
